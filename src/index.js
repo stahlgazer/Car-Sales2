8cebfb9f20f14fb4e55e5d60772d8727e51f1fa2
gavin stahl <stahlgavin@gmail.com>
2020-02-18 13:07:02 -0800

    files created, store setup, initial state created

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles.scss";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { featureReducer } from "./reducers";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+const store = createStore(featureReducer);
+console.log(store.getState());
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
